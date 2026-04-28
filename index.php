2300313c32ed17d12da182af4dd41c4ad856109f
rootameli <rootameli@gmail.com>
2026-04-28 18:40:22 +0200

    Remove obfuscated webshell components and sanitize secure entrypoint

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 
-    include 'secure/anti1.php';
-	include 'secure/anti2.php';
-	include 'secure/anti3.php';
-	include 'secure/anti4.php';
-	include 'secure/anti5.php';
-	include 'secure/anti6.php';
-	include 'secure/anti7.php';
-	include 'secure/anti8.php';
 	include 'vendor/random.php';
 	include 'vendor/seen.php';
-	include 'vendor/forbidden.php';
 
    // To banned IP Bots Go to => vendor/ban.txt and put your IPS.
    $file = file_get_contents( "vendor/ban.txt" );
